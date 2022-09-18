db9baeddb894a3e6faca2cfffe19fadec4f51639
roman_bv <ruff18@mail.ru>
2022-09-18 18:24:35 +0300

    создание нового приложения labsite

--- labsite/settings.py
+++ labsite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'polls.apps.PollsConfig',
+
 ]
 
 MIDDLEWARE = [
