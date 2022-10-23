0dbe001eea5c32af00f4e47825bf2d17ffd97291
roman_bv <ruff18@mail.ru>
2022-10-23 20:07:31 +0300

    add whitenoise

--- labsite/settings.py
+++ labsite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR.joinpath("static")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR.joinpath("media")
 MEDIA_URL = '/media/'
